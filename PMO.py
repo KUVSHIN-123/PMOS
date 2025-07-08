ccd162f919ce2c1d10f41d7f47b9f54f3db3f769
kuvshin <kirikgorbacev@gmail.com>
2025-07-08 20:39:26 +0300

    0.0.1

--- PMO.py
+++ PMO.py
@@ -3,4 +3,18 @@ import zmq
 def publisher_data():
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
-    socket.bind()
+    socket.bind("tcp://desktop-43mqlbj-1.netbird.cloud:2000")
+    while True:
+        data = input()
+        message = f"testing_topic {data}"
+        socket.send_string(message)
+        print(f"Data: {data} send!")
+
+def subscriber_data():
+    context = zmq.Context()
+    socket = context.socket(zmq.SUB)
+    socket.connect("tcp://desktop-43mqlbj-1.netbird.cloud:2000")
+    socket.setsockopt_string(zmq.SUBSCRIBE,"testing_topic")
+    while True:
+        response = socket.recv_string()
+        print(f"Response: {response}")
